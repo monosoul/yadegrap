45c6d842b0a6290ad2b749205cd61d0b442f4b36
monosoul <kloz.klaud@gmail.com>
2019-04-13 23:37:20 +0500

    add gradle plugin publish configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,31 @@ repositories {
 kotlinDslPluginOptions {
     experimentalWarning.set(false)
 }
+
+gradlePlugin {
+    plugins {
+        create("yetAnotherDelombokGradlePlugin") {
+            id = "yadegrap"
+            implementationClass = "com.github.monosoul.yadegrap.YetAnotherDelombokGradlePlugin"
+        }
+    }
+}
+
+pluginBundle {
+    (plugins) {
+        "yetAnotherDelombokGradlePlugin" {
+            displayName = "Yet Another Delombok Gradle Plugin"
+            description = "This plugin provides a configurable delombok task that uses Ant delombok task."
+            tags = listOf("lombok", "delombok", "java")
+            version = project.version as String
+
+            website = "https://github.com/monosoul/yadegrap"
+            vcsUrl = "https://github.com/monosoul/yadegrap"
+        }
+    }
+
+    mavenCoordinates {
+        artifactId = project.name
+        version = project.version as String
+    }
+}
