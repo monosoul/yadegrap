397dd2ad241eab73f482702f9e0c3d910ec75486
monosoul <kloz.klaud@gmail.com>
2019-04-14 00:55:08 +0500

    add KDoc; fix file name

--- src/main/kotlin/com/github/monosoul/yadegrap/DelombokTask.kt
+++ src/main/kotlin/com/github/monosoul/yadegrap/DelombokTask.kt
@@ -8,6 +8,9 @@ import org.gradle.kotlin.dsl.withGroovyBuilder
 import java.io.File
 import org.gradle.api.tasks.SourceSet.MAIN_SOURCE_SET_NAME as MAIN
 
+/**
+ * Task that preforms delombok operation on the provided sources.
+ */
 open class DelombokTask : DefaultTask() {
     init {
         group = "lombok"
@@ -16,40 +19,78 @@ open class DelombokTask : DefaultTask() {
 
     private val main = (project.properties["sourceSets"] as SourceSetContainer).getByName<SourceSet>(MAIN)
 
+    /**
+     * Path to sources that should be delomboked.
+     *
+     * By default uses the first source directory of main's java sources.
+     */
     @InputDirectory
     var inputDir: File = main.java.srcDirs.first()
 
+    /**
+     * Path to the destination directory where delomboked sources are going to be placed.
+     *
+     * By default uses "delomboked" dir in the project's buildDir.
+     */
     @OutputDirectory
     var outputDir: File = File(project.buildDir, "delomboked")
 
+    /**
+     * Path to lombok.jar.
+     *
+     * By default tries to find it in the main's compile classpath.
+     */
     @InputFile
     var pathToLombok: File = main.compileClasspath
             .filter { it.name.startsWith("lombok") && it.name.endsWith(".jar") }
             .first()
 
+    /**
+     * Class path to be used during the execution of delmbok task. Should have all the classes used by the sources
+     * which are going to be delomboked.
+     *
+     * By default uses main's compile classpath.
+     */
     @Input
     var classPath: String = main.compileClasspath.asPath
 
+    /**
+     * Source path to be used during the execution of delmbok task.
+     */
     @Input
     @Optional
     var sourcePath: String? = null
 
+    /**
+     * Module path to be used during the execution of delmbok task.
+     */
     @Input
     @Optional
     var modulePath: String? = null
 
+    /**
+     * Files' encoding.
+     *
+     * Default: UTF-8
+     */
     @Input
     var encoding: String = "UTF-8"
 
+    /**
+     * Logging verbosity.
+     *
+     * Default: false
+     */
     @Input
     var verbose: Boolean = false
 
+    /**
+     * Formatting options for the delombok task.
+     *
+     * By default is empty.
+     */
     @Input
-    var formatOptions: Map<String, String> = mapOf(
-            "generateDelombokComment" to "skip",
-            "generated" to "generate",
-            "javaLangAsFQN" to "skip"
-    )
+    var formatOptions: Map<String, String> = mapOf()
 
     @TaskAction
     fun run() {
