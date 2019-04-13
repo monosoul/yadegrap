6cbb7ec785e9291df34d907c0e65f3817e92b5dd
monosoul <kloz.klaud@gmail.com>
2019-04-13 23:36:57 +0500

    Introduce a YetAnotherDelombokGradlePlugin

---
+++ src/main/kotlin/com/github/monosoul/yadegrap/YetAnotherDelombokGradlePlugin.kt
@@ -0,0 +1,15 @@
+package com.github.monosoul.yadegrap
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.*
+
+class YetAnotherDelombokGradlePlugin : Plugin<Project> {
+
+    override fun apply(project: Project): Unit = project.run {
+
+        tasks {
+            register("delombok", DelombokTask::class)
+        }
+    }
+}
